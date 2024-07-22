d243fd7a078381e9297435c9d483efd438e60612
LucindaBotes <Lucinda.Botes@bbd.co.za>
2024-07-22 16:43:02 +0200

    DA-5: try to create a network

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,10 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+// import * as managedblockchain from 'aws-cdk-lib/aws-managedblockchain';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // const network = new managedblockchain.CfnMember(this, 'MyCfnMember' {
+    //   memberConfiguration: {
+    //     name: 'MyMember',
+    //     networkId: 'n-abc123',
+    //     frameworkConfiguration: {
+    //       fabric: {
+    //         adminPassword: 'MyPassword'
+    //       }
+    //     }
+    //   }
+    // });
 
     // The code that defines your stack goes here
 
